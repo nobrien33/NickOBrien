dd9b9e8503aea36598c2093b599bf25c4d5ea80b
nobrien33 <nobrien2@wisc.edu>
2018-04-10 07:57:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -185,16 +185,27 @@ function makeColorScale(data){
     ];
 
     //create color scale generator
-    var colorScale = d3.scaleQuantile()
+    var colorScale = d3.scaleThreshold()
         .range(colorClasses);
 
-    //build two-value array of minimum and maximum expressed attribute values
-    var minmax = [
-        d3.min(data, function(d) { return parseFloat(d[expressed]); }),
-        d3.max(data, function(d) { return parseFloat(d[expressed]); })
-    ];
-    //assign two-value array as scale domain
-    colorScale.domain(minmax);
+    //build array of all values of the expressed attribute
+    var domainArray = [];
+    for (var i=0; i<data.length; i++){
+        var val = parseFloat(data[i][expressed]);
+        domainArray.push(val);
+    };
+
+    //cluster data using ckmeans clustering algorithm to create natural breaks
+    var clusters = ss.ckmeans(domainArray, 5);
+    //reset domain array to cluster minimums
+    domainArray = clusters.map(function(d){
+        return d3.min(d);
+    });
+    //remove first value from domain array to create class breakpoints
+    domainArray.shift();
+
+    //assign array of last 4 cluster minimums as domain
+    colorScale.domain(domainArray);
 
     return colorScale;
     
